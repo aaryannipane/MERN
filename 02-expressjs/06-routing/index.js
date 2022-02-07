a315cbc541c59c70da44faad3b889542982326ed
Aryan Nipane <nipaneeducation@gmail.com>
2022-02-08 02:13:37 +0530

    express routing

---
+++ 02-expressjs/06-routing/index.js
@@ -0,0 +1,79 @@
+// const express = require('express');
+import express from 'express' 
+const app = express();
+const port = 3000;
+
+// ROUTING
+
+// normal string path
+app.get('/home', (req, res)=>{
+    res.send('Hello This is routing');
+})
+
+// routing for all http request
+// app.all('/sabkuch', (req, res)=>{
+//     res.send('All method')
+// })
+
+// routing for page not found 
+// app.all('*', (req, res)=>{
+//     res.send('page not found');
+// })
+
+
+// prefix routing
+// app.all('/api/*', (req, res)=>{
+//     res.send('API Page');
+// })
+
+
+// string pattern path
+// app.get('/ab?cd', (req, res)=>{
+//     res.send("this route path will match acd and abcd");
+// })
+
+// regular expression path
+// app.get(/a/, (req, res)=>{
+//     res.send('this is a');
+// })
+
+// callback examples
+app.get('/', (req, res)=>{
+    res.send('One callback');
+})
+
+
+// callback examples
+// app.get('/callback2', (req, res, next)=>{
+//     console.log('first callback');
+//     next();
+// }, (req, res)=>{
+//     console.log('second callback')
+//     res.send('Second callback')
+// })
+
+
+// an array of callback
+const c1 = (req, res, next)=>{
+    console.log('First Callback');
+    next();
+}
+const c2 = (req, res, next)=>{
+    console.log('Second Callback');
+    next();
+}
+const c3 = (req, res)=>{
+    res.send('This is array of callback');
+}
+
+app.get('/callback3', [c1, c2, c3]);
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log("server is listening at port "+ port);
+});
