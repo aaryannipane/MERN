1c1e81af4192b613b6a897b88b7c5d697bb1276d
Aryan Nipane <nipaneeducation@gmail.com>
2022-06-26 18:52:08 +0530

    removed db files

--- 02-expressjs/17-mongoose-with-express/02-connect-express-to-mongoDB-using-mongoose/db/connectdb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = ()=>{
-    return mongoose.connect("mongodb+srv://root:root@nodeapi.c1c5h.mongodb.net/schooldb?retryWrites=true&w=majority", ()=>{
-        console.log("Connected with database");
-    })
-}
-
-export default connectDB
